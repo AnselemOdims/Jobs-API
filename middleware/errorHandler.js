a3ee6ca821c41206e9ddc22d5a26e7a8e09e85f1
Anselem Odimegwu <odimegwu@systemspecs.com.ng>
2023-03-31 14:02:27 +0100

    Feat: setup duplicate and validation error handlers

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,11 +1,29 @@
-const { CustomError } = require('../utils/CustomError');
-
 const errorHandler = (err, req, res, next) => {
-    if(err instanceof CustomError) {
-        res.status(err.statusCode).json({ code: '99', err: err, msg: err.message})
-    } else {
-        res.status(500).json({ code: '99', err })
+  let customError = {
+    message: err?.message ?? 'Something went wrong, please try again!',
+    statusCode: err.statusCode || 500,
+  };
+  if (err.code && err.code === 11000) {
+    customError = {
+      message: `Duplicate error for the ${Object.keys(
+        err.keyValue
+      )} field. Please choose a different value`,
+      statusCode: 400,
+    };
+  }
+  if(err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map(item => item.message).join(', ')
+    // const keys = Object.keys(err.errors).join(', and ')
+    // const message = `Please provide ${keys}`
+    customError = {
+        message,
+        statusCode: 400
     }
-}
+  }
+//   res.status(500).json({ err })
+  res
+    .status(customError.statusCode)
+    .json({ code: '99', message: customError.message });
+};
 
 module.exports = errorHandler;
